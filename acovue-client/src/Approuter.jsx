5eeb529d0ff0c24ff745d89b2d47a287ab041d58
WhooGeek <rlarjswn0303@naver.com>
2025-12-20 18:46:21 +0900

    FEAT: 라우터 설정

--- acovue-client/src/Approuter.jsx
+++ acovue-client/src/Approuter.jsx
@@ -1,6 +1,8 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
+import NewsListPage from "./pages/News/NewsListPage";
+import BehindListPage from "./pages/Behind/BehindListPage"
 import NewsDetailPage from "./pages/News/NewsDetailPage"
 import BehindDetailpage from "./pages/Behind/BehindDetailPage"
 import CommunityDetailPage from "./pages/Community/CommunityDetailPage"
@@ -10,6 +12,8 @@ export default function AppRouter() {
         <Routes>
             <Route path="/" element={<Home />}/>
             <Route path="/about_me/" element={<About/>}/>
+            <Route path="/news/" element={<NewsListPage/>} />
+            <Route path="/behind/" element={<BehindListPage/>} />
             <Route path="/news/:postId/" element={<NewsDetailPage />} />
             <Route path="/behind/:postId/" element={<BehindDetailpage/>} />
             <Route path="/community/:postId/" element={<CommunityDetailPage/>} />
